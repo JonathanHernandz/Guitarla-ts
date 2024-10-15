2b787db26c95916867acb014a43d107aa4a4d463
Jonathan <electroc_jonathan@hotmail.com>
2024-10-15 16:39:19 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Guitar from "./components/Guitar"
+import Header from "./components/Header.TSX"
+import { db } from "./data/db";
+import { useCart } from "./hooks/useCart";
+
+function App() {
+  const { data, cart, addToCart, removeFromCart, decrementQuantity, increaseQuantity, clearCart, isEmpty, cartTotal} = useCart();
+    
+  // const [auth, setAuth] = useState(true); 
+  
+
+    // function saveLocalStorage(){
+    //   localStorage.setItem('cart',JSON.stringify(cart));
+    // }
+  return (
+    <>
+      <Header
+        cart = {cart}
+        removeFromCart = {removeFromCart}
+        increaseQuantity = {increaseQuantity}
+        decrementQuantity = {decrementQuantity}
+        clearCart = {clearCart}
+        isEmpty = {isEmpty}
+        cartTotal = {cartTotal}
+      />
+
+      <main className="container-xl mt-5">
+          <h2 className="text-center">Nuestra Colección</h2>
+          <div className="row mt-5">
+            {
+                data.map((guitar) =>(
+                    <Guitar
+                        key={guitar.id}
+                        guitar = {guitar}
+                        //cart={cart}
+                        //setCart = {setCart}
+                        addToCart={addToCart}
+                    />
+                ) )
+            }
+                   
+          </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+          <div className="container-xl">
+              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          </div>
+      </footer>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/hooks/useCart.ts
@@ -0,0 +1,91 @@
+import { useState, useEffect, useMemo } from "react"
+import { db } from "../data/db";
+
+export const useCart = () =>{
+    // console.log("desde useCart");
+
+    const initialCart = () =>{
+        const localStorageCart = localStorage.getItem('cart');
+        return localStorageCart ? JSON.parse(localStorageCart) : [];
+      }
+  
+      const [data] = useState(db);
+      const [cart, setCart] = useState(initialCart);
+  
+      const MAX_ITEMS = 10;
+  
+      useEffect(() => {
+        localStorage.setItem('cart',JSON.stringify(cart));
+      }, [cart])
+      
+  
+      function addToCart(item){
+         const itemExist = cart.findIndex( (guitar)=> guitar.id === item.id );
+        if(itemExist >= 0) { //Existe en el carrito
+          if(cart[itemExist].quantity >= MAX_ITEMS) return;
+          console.log("Ya existe en el carrito");
+          const updateCart = [...cart];
+          updateCart[itemExist].quantity++;
+          setCart(updateCart);
+        }else{
+          console.log("Agregando");
+          item.quantity = 1;
+          //setCart( (prevCart) => [...prevCart, item]);
+          setCart([...cart, item]); //code mas corto
+        }
+        
+        
+        // saveLocalStorage(); 
+      }
+      
+      function removeFromCart(id){
+        // console.log("Eliminando articulo....",id);
+        setCart( prevCart => prevCart.filter( guitar => guitar.id !== id ) )
+      }
+      function increaseQuantity(id) {
+        // console.log("incrementando articulo",id);}
+        const updateCart = cart.map( item => { 
+          if(item.id === id && item.quantity < MAX_ITEMS){
+            return{
+              ...item,
+              quantity: item.quantity + 1
+            }
+          }
+          return item;
+        })
+        setCart(updateCart);
+      }
+  
+      function decrementQuantity(id){
+        const updateCart = cart.map( item => {
+          if(item.id === id && item.quantity > 1){
+            return{
+              ...item,
+              quantity: item.quantity - 1
+            }
+          }
+          return item;
+        })
+        setCart(updateCart);
+      }
+  
+      function clearCart(){
+        setCart([]);
+  
+      }
+
+    const isEmpty = useMemo( () => cart.length === 0 , [cart]);
+    const cartTotal =useMemo(() => cart.reduce( (total,item) => total + (item.quantity * item.price), 0 ),[cart]); 
+
+    return {
+        data, 
+        cart, 
+        addToCart, 
+        removeFromCart, 
+        decrementQuantity, 
+        increaseQuantity,
+        clearCart,
+        isEmpty,
+        cartTotal
+    }
+}
